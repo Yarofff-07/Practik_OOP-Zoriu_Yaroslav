9f288b1e51bc0e1294099da4fc5cd0021128d9ca
Yarofff <zoriu.ayroslav2007@gmail.com>
2026-03-05 18:24:05 +0200

    Delete Prac1.java

--- Prac1.java
+++
@@ -1,9 +0,0 @@
-public class Prac1 {
-    public static void main(String[] args) {
-        System.out.println("Кількість аргументів: " + args.length);
-
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("Аргумент " + i + ": " + args[i]);
-        }
-    }
-}
